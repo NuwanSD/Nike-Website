93a34d20391ddbb8b2573d78ffaad48c198054cf
Sagara Nuwan <sagaradnuwan@gmail.com>
2023-09-15 13:13:03 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
